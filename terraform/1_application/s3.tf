946dbd9f072fe0dd7b05ab3c30c89755c933d131
Pavel Pikta <pavel_pikta@epam.com>
2022-09-09 16:49:48 +0300

    Add s3 bucket for lambda builds

--- terraform/1_application/s3.tf
+++ terraform/1_application/s3.tf
@@ -1,7 +1,34 @@
+####################################
+# S3 bucket that store lambda builds
+####################################
+module "s3_lambda_builds" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.4.0"
+
+  bucket = "${var.project}-${var.environment}-lambda-builds"
+  acl    = "private"
+
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  versioning = {
+    enabled = false
+  }
+
+  tags = {
+    "Service" = "S3"
+  }
+}
+
 ##############################
 # S3 bucket that store dataset
 ##############################
-
 module "s3_dataset_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "3.4.0"
