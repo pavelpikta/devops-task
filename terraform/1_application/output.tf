6b216bd777ce8378f90570c45279731510d900d9
Pavel Pikta <pavel_pikta@epam.com>
2022-09-09 17:41:16 +0300

    Add outputs

--- terraform/1_application/output.tf
+++ terraform/1_application/output.tf
@@ -1,9 +1,24 @@
-output "dataset_s3_bucket_arn" {
-  description = "Dataset AWS S3 bucket ARN"
-  value       = module.s3_dataset_bucket.s3_bucket_arn
-}
-
 output "dataset_s3_bucket_name" {
   description = "Dataset AWS S3 bucket name"
   value       = module.s3_dataset_bucket.s3_bucket_id
+}
+
+output "static_pages_s3_bucket_name" {
+  description = "Static page AWS S3 bucket name"
+  value       = module.s3_static_page.s3_bucket_id
+}
+
+output "lambda_builds_s3_bucket_name" {
+  description = "AWS S3 bucket name for lambda buils"
+  value       = module.s3_lambda_builds.s3_bucket_id
+}
+
+output "static_page_endpoint" {
+  description = "The website endpoint"
+  value       = try(module.s3_static_page.s3_bucket_website_endpoint, "")
+}
+
+output "static_page_url" {
+  description = "The website url"
+  value       = try("http://${module.s3_static_page.s3_bucket_website_endpoint}", "")
 }
